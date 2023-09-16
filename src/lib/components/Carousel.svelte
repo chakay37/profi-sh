8803bd8e21c8cfa67b64dee4d39d6bc1baec7d0d
NamNam37 <35643092+NamNam37@users.noreply.github.com>
2023-09-16 11:55:44 +0200

    width fix + bootstrap opt

---
+++ src/lib/components/Carousel.svelte
@@ -0,0 +1,89 @@
+<script lang="ts">
+	import firstShowcase from '$lib/main-img/1.jpg';
+	import secondShowcase from '$lib/main-img/2.jpg';
+	import thirdShowcase from '$lib/main-img/3.jpg';
+	import './carousel-bs.css';
+</script>
+
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+</head>
+<body>
+	<div class="carousel-container">
+		<div id="carouselExampleInterval" class="carousel slide carousel-fade" data-bs-ride="carousel">
+			<div class="carousel-inner">
+				<div class="carousel-item active" data-bs-interval="4000">
+					<img src={firstShowcase} class="d-block w-100 img-fluid" alt="obrázek oblečení" />
+				</div>
+				<div class="carousel-item" data-bs-interval="4000">
+					<img src={secondShowcase} class="d-block w-100 img-fluid" alt="obrázek oblečení" />
+				</div>
+				<div class="carousel-item" data-bs-interval="4000">
+					<img src={thirdShowcase} class="d-block w-100 img-fluid" alt="obrázek oblečení" />
+				</div>
+			</div>
+			<button
+				class="carousel-control-prev"
+				type="button"
+				data-bs-target="#carouselExampleInterval"
+				data-bs-slide="prev"
+			>
+				<span class="carousel-control-prev-icon" aria-hidden="true" />
+				<span class="visually-hidden">Previous</span>
+			</button>
+			<button
+				class="carousel-control-next"
+				type="button"
+				data-bs-target="#carouselExampleInterval"
+				data-bs-slide="next"
+			>
+				<span class="carousel-control-next-icon" aria-hidden="true" />
+				<span class="visually-hidden">Next</span>
+			</button>
+		</div>
+	</div>
+
+	<script
+		src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+		crossorigin="anonymous"
+	></script>
+</body>
+
+<style lang="scss">
+	.carousel-container {
+		width: 100%;
+		overflow-y: hidden;
+		height: 350px;
+		div {
+			height: 100%;
+		}
+		.carousel-item {
+			width: 100%;
+		}
+		img {
+			height: 100%;
+			width: 100%;
+			object-fit: cover;
+			border: black 2px solid;
+			box-sizing: border-box;
+			border-radius: 10px;
+		}
+		button {
+			opacity: 1;
+		}
+	}
+	@media (max-width: 1400px) {
+		.carousel-container {
+			width: 100%;
+			height: 200px;
+			div {
+				height: 100%;
+			}
+			img {
+				height: 100%;
+			}
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
 	import './style.scss';
+	import Carousel from '$lib/components/Carousel.svelte';
+	import { browser } from '$app/environment';
 
-	import firstShowcase from '$lib/main-img/1.jpg';
-	import secondShowcase from '$lib/main-img/2.jpg';
-	import thirdShowcase from '$lib/main-img/3.jpg';
 	import staticImage from '$lib/main-img/4.jpg';
 	/*const staticImage = new URL(
 		'https://cdn.atomix.vg/wp-content/uploads/2015/07/super-mario-maker-facebook.jpg'
@@ -23,7 +22,6 @@
 	import ig4 from '$lib/main-img/ig4.jpg';
 
 	import SectTitle from '$lib/components/Title.svelte';
-
 	import { onMount } from 'svelte';
 	/*import axios from 'axios';
 
@@ -48,220 +46,165 @@
 	];
 </script>
 
-<html lang="en">
-	<body>
-		<main>
-			<section class="title-sect">
-				<div class="title">
-					<h1>Profi Second Hand</h1>
-				</div>
-				<div class="carousel-container">
-					<div
-						id="carouselExampleInterval"
-						class="carousel slide carousel-fade"
-						data-bs-ride="carousel"
-					>
-						<div class="carousel-inner">
-							<div class="carousel-item active" data-bs-interval="4000">
-								<img
-									src={firstShowcase}
-									class="d-block w-100 img-responsive"
-									alt="obrázek oblečení"
-								/>
-							</div>
-							<div class="carousel-item" data-bs-interval="4000">
-								<img
-									src={secondShowcase}
-									class="d-block w-100 img-responsive"
-									alt="obrázek oblečení"
-								/>
-							</div>
-							<div class="carousel-item" data-bs-interval="4000">
-								<img
-									src={thirdShowcase}
-									class="d-block w-100 img-responsive"
-									alt="obrázek oblečení"
-								/>
-							</div>
-						</div>
-						<button
-							class="carousel-control-prev"
-							type="button"
-							data-bs-target="#carouselExampleInterval"
-							data-bs-slide="prev"
-						>
-							<span class="carousel-control-prev-icon" aria-hidden="true" />
-							<span class="visually-hidden">Previous</span>
-						</button>
-						<button
-							class="carousel-control-next"
-							type="button"
-							data-bs-target="#carouselExampleInterval"
-							data-bs-slide="next"
-						>
-							<span class="carousel-control-next-icon" aria-hidden="true" />
-							<span class="visually-hidden">Next</span>
-						</button>
-					</div>
-				</div>
-			</section>
-			<section class="deals-sect">
-				<SectTitle>Nové akce</SectTitle>
-				<div class="table-container table-responsive">
-					<table class="table table-striped table-hover">
-						<thead>
-							<tr>
-								<th class="corner" scope="col" />
-								<th scope="col">Dnes</th>
-								<th scope="col">Zítra</th>
-								<th scope="col">So 26.8.</th>
-								<th scope="col">Ne 27.8.</th>
-								<th scope="col">Po 28.8.</th>
-								<th scope="col">Út 29.8.</th>
-								<th scope="col">St 30.8.</th>
-								<th scope="col">Čt 1.9.</th>
-								<th scope="col">Pá 2.9.</th>
-								<th scope="col">So 3.9.</th>
-								<th scope="col">Ne 4.9.</th>
-							</tr>
-						</thead>
-						<tbody>
-							{#each citiesArr as city}
-								<tr>
-									<th scope="row">{city}</th>
-									<td
-										><div class="table-item">
-											<p>-30%</p>
-											<a href="">Zjistit více <img src={linkIcon} alt="Odkaz na akci" /></a>
-										</div></td
-									>
-									<td
-										><div class="table-item">
-											<p>-20%</p>
-											<a href="">Zjistit více <img src={linkIcon} alt="Odkaz na akci" /></a>
-										</div></td
-									>
-									<td />
-									<td />
-									<td />
-									<td />
-									<td />
-									<td />
-									<td />
-									<td />
-									<td />
-								</tr>
-							{/each}
-						</tbody>
-					</table>
-				</div>
-			</section>
-			<section class="about-sect">
-				<img class="only-desktop" src={staticImage} alt="obrázek oblečení" />
-				<div class="text">
-					<SectTitle>O nás</SectTitle>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, in dolor. Nobis,
-						reprehenderit expedita velit voluptatum magnam obcaecati modi provident a alias dolorum,
-						minus doloremque maxime deserunt itaque labore molestias.
-					</p>
-				</div>
-			</section>
-			<section class="blog-sect">
-				<SectTitle>Second Hand a ekologie</SectTitle>
-				<p>
-					Rychlá móda má dopad na planetu. Zajímáte se o ekologii? Nyní máte možnost zjisti více.
-					Second hand nabízí stylová a udržitelná řešení. Podpořte životní prostředí svým výběrem.
-				</p>
-				<div class="shadow" />
-				<button class="primary-button">Zjistit více </button>
-				<img src={branch1} alt="větev" id="branch1" class="only-desktop" />
-				<img src={branch2} alt="větev" id="branch2" class="only-desktop" />
-				<img src={branch3} alt="větev" id="branch3" class="only-desktop" />
-			</section>
-			<section class="notification-sect">
-				<div class="info">
-					<SectTitle>Dostávejte upozornění na akce a slevy</SectTitle>
-					<p>
-						Chcete být informováni o akcích ve Vašem okolí pomocí E-Mailu a SMS? <br /> Zaregistrujte
-						se!
-					</p>
-				</div>
-				<form action="" method="get">
-					<div class="mb-3">
-						<label class="form-label"
-							>E-mail
-							<input type="email" class="form-control" placeholder="jannovak@seznam.cz" />
-						</label>
-					</div>
-					<div class="mb-3">
-						<label class="form-label"
-							>Tel. číslo
-							<input type="phone" class="form-control" placeholder="+420 777 645 189" />
-						</label>
-					</div>
-					<div class="mb-3">
-						<label class="form-label"
-							>Město
-							<select class="form-select">
-								{#each citiesArr as city, i}
-									<option value={i + 1}>{city}</option>
-								{/each}
-							</select>
-						</label>
-					</div>
-					<button class="secondary-button" type="submit">Potvrdit</button>
-				</form>
-			</section>
-			<section class="socials-sect">
-				<SectTitle>Sociální sítě</SectTitle>
-				<div class="socials-row" id="only-desktop">
-					<div class="item-container fb">
-						<a href="https://www.facebook.com/profisecondhand">
-							<img src={fb1} alt="facebook profi sh" />
-							<img src={facebookIcon} alt="facebook profi sh" class="icon" />
-						</a>
-					</div>
-					<div class="item-container ig">
-						<a href="https://www.instagram.com/profi_second_hand/">
-							<img src={ig1} alt="instagram profi sh" />
-							<img src={instagramIcon} alt="instagram profi sh" class="icon" />
-						</a>
-					</div>
-					<div class="item-container fb">
-						<a href="https://www.facebook.com/profisecondhand">
-							<img src={fb2} alt="facebook profi sh" />
-							<img src={facebookIcon} alt="facebook profi sh" class="icon" />
-						</a>
-					</div>
-				</div>
-				<div class="socials-row">
-					<div class="item-container ig">
-						<a href="https://www.instagram.com/profi_second_hand/">
-							<img src={ig2} alt="facebook profi sh" />
-							<img src={instagramIcon} alt="instagram profi sh" class="icon" />
-						</a>
-					</div>
-					<div class="item-container fb">
-						<a href="https://www.facebook.com/profisecondhand">
-							<img src={fb3} alt="facebook profi sh" />
-							<img src={facebookIcon} alt="facebook profi sh" class="icon" />
-						</a>
-					</div>
-					<div class="item-container ig">
-						<a href="https://www.instagram.com/profi_second_hand/">
-							<img src={ig3} alt="facebook profi sh" />
-							<img src={instagramIcon} alt="instagram profi sh" class="icon" />
-						</a>
-					</div>
-					<div class="item-container ig">
-						<a href="https://www.instagram.com/profi_second_hand/">
-							<img src={ig4} alt="facebook profi sh" />
-							<img src={instagramIcon} alt="instagram profi sh" class="icon" />
-						</a>
-					</div>
-				</div>
-			</section>
-		</main>
-	</body>
-</html>
+<main>
+	<section class="title-sect">
+		<div class="title">
+			<h1>Profi Second Hand</h1>
+		</div>
+		<div class="carousel-bs"><Carousel /></div>
+	</section>
+	<section class="deals-sect">
+		<SectTitle>Nové akce</SectTitle>
+
+		<div class="table-container table-responsive">
+			<table class="table table-striped table-hover">
+				<thead>
+					<tr>
+						<th class="corner" scope="col" />
+						<th scope="col">Dnes</th>
+						<th scope="col">Zítra</th>
+						<th scope="col">So 26.8.</th>
+						<th scope="col">Ne 27.8.</th>
+						<th scope="col">Po 28.8.</th>
+						<th scope="col">Út 29.8.</th>
+						<th scope="col">St 30.8.</th>
+						<th scope="col">Čt 1.9.</th>
+						<th scope="col">Pá 2.9.</th>
+						<th scope="col">So 3.9.</th>
+						<th scope="col">Ne 4.9.</th>
+					</tr>
+				</thead>
+				<tbody>
+					{#each citiesArr as city}
+						<tr>
+							<th scope="row">{city}</th>
+							<td
+								><div class="table-item">
+									<p>-30%</p>
+									<a href="">Zjistit více <img src={linkIcon} alt="Odkaz na akci" /></a>
+								</div></td
+							>
+							<td
+								><div class="table-item">
+									<p>-20%</p>
+									<a href="">Zjistit více <img src={linkIcon} alt="Odkaz na akci" /></a>
+								</div></td
+							>
+							<td />
+							<td />
+							<td />
+							<td />
+							<td />
+							<td />
+							<td />
+							<td />
+							<td />
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+		</div>
+	</section>
+	<section class="about-sect">
+		<img class="only-desktop" src={staticImage} alt="obrázek oblečení" />
+		<div class="text">
+			<SectTitle>O nás</SectTitle>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, in dolor. Nobis,
+				reprehenderit expedita velit voluptatum magnam obcaecati modi provident a alias dolorum,
+				minus doloremque maxime deserunt itaque labore molestias.
+			</p>
+		</div>
+	</section>
+	<section class="blog-sect">
+		<SectTitle>Second Hand a ekologie</SectTitle>
+		<p>
+			Rychlá móda má dopad na planetu. Zajímáte se o ekologii? Nyní máte možnost zjisti více. Second
+			hand nabízí stylová a udržitelná řešení. Podpořte životní prostředí svým výběrem.
+		</p>
+		<div class="shadow" />
+		<button class="primary-button">Zjistit více </button>
+		<img src={branch1} alt="větev" id="branch1" class="only-desktop" />
+		<img src={branch2} alt="větev" id="branch2" class="only-desktop" />
+		<img src={branch3} alt="větev" id="branch3" class="only-desktop" />
+	</section>
+	<section class="notification-sect">
+		<div class="info">
+			<SectTitle>Dostávejte upozornění na akce a slevy</SectTitle>
+			<p>
+				Chcete být informováni o akcích ve Vašem okolí pomocí E-Mailu a SMS? <br /> Zaregistrujte se!
+			</p>
+		</div>
+		<form action="" method="get">
+			<label
+				>E-mail
+				<input type="email" placeholder="jannovak@seznam.cz" />
+			</label>
+
+			<label
+				>Tel. číslo
+				<input type="phone" placeholder="+420 777 645 189" />
+			</label>
+
+			<label
+				>Město
+				<select>
+					{#each citiesArr as city, i}
+						<option value={i + 1}>{city}</option>
+					{/each}
+				</select>
+			</label>
+
+			<button class="secondary-button" type="submit">Potvrdit</button>
+		</form>
+	</section>
+	<section class="socials-sect">
+		<SectTitle>Sociální sítě</SectTitle>
+		<div class="socials-row" id="only-desktop">
+			<div class="item-container fb">
+				<a href="https://www.facebook.com/profisecondhand">
+					<img src={fb1} alt="facebook profi sh" />
+					<img src={facebookIcon} alt="facebook profi sh" class="icon" />
+				</a>
+			</div>
+			<div class="item-container ig">
+				<a href="https://www.instagram.com/profi_second_hand/">
+					<img src={ig1} alt="instagram profi sh" />
+					<img src={instagramIcon} alt="instagram profi sh" class="icon" />
+				</a>
+			</div>
+			<div class="item-container fb">
+				<a href="https://www.facebook.com/profisecondhand">
+					<img src={fb2} alt="facebook profi sh" />
+					<img src={facebookIcon} alt="facebook profi sh" class="icon" />
+				</a>
+			</div>
+		</div>
+		<div class="socials-row">
+			<div class="item-container ig">
+				<a href="https://www.instagram.com/profi_second_hand/">
+					<img src={ig2} alt="facebook profi sh" />
+					<img src={instagramIcon} alt="instagram profi sh" class="icon" />
+				</a>
+			</div>
+			<div class="item-container fb">
+				<a href="https://www.facebook.com/profisecondhand">
+					<img src={fb3} alt="facebook profi sh" />
+					<img src={facebookIcon} alt="facebook profi sh" class="icon" />
+				</a>
+			</div>
+			<div class="item-container ig">
+				<a href="https://www.instagram.com/profi_second_hand/">
+					<img src={ig3} alt="facebook profi sh" />
+					<img src={instagramIcon} alt="instagram profi sh" class="icon" />
+				</a>
+			</div>
+			<div class="item-container ig">
+				<a href="https://www.instagram.com/profi_second_hand/">
+					<img src={ig4} alt="facebook profi sh" />
+					<img src={instagramIcon} alt="instagram profi sh" class="icon" />
+				</a>
+			</div>
+		</div>
+	</section>
+</main>
