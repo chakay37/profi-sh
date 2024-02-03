24413312ecf5bb934e3c9cc8b1a1e0083539e841
NamNam37 <35643092+NamNam37@users.noreply.github.com>
2024-02-03 21:13:28 +0100

    presunuti uploadu photos a admin panel

---
+++ src/lib/photos.svelte
@@ -0,0 +1,74 @@
+<script lang="ts" context="module">
+	import { encode, decode } from 'js-base64';
+	import { compress } from 'image-conversion';
+
+	const dontReadThisPls = encode('SF/4OVvE5KvU57QNjYz4y0IlMMx7IhhvFbFSO5WY');
+
+	export const handleSubmit = async (e: SubmitEvent, fileName: string) => {
+		e.preventDefault();
+
+		const formData = new FormData(e.target as HTMLFormElement);
+		const file = formData.get('file') as Blob;
+
+		// Assuming 'file' is your PNG image file
+		let jpgBlob: Blob;
+		await compress(file, {
+			quality: 0.8,
+			type: 'image/jpeg'
+		}).then((res: Blob) => {
+			console.log(res);
+			jpgBlob = res;
+		});
+
+		AWS.config.update({
+			accessKeyId: 'AKIAWQZV5LCLPYYRFKGN',
+			secretAccessKey: decode(dontReadThisPls),
+			region: 'us-east-1'
+		});
+
+		const s3 = new AWS.S3();
+
+		const params = {
+			Bucket: 'file-upload-sh',
+			Key: fileName + '.jpg',
+			Body: jpgBlob
+		};
+
+		s3.upload(params, (err, data) => {
+			if (err) {
+				console.error('Error uploading file:', err);
+			} else {
+				console.log('File uploaded successfully. S3 URL:', data.Location);
+			}
+		});
+		/*const url = 'https://5gkaf9hvg6.execute-api.us-east-1.amazonaws.com/photos';
+    const formData = new FormData(e.target as HTMLFormElement);
+
+    const fileInputs = formData.getAll('file');
+
+    let photos = [];
+
+    for (const fileInput of fileInputs) {
+        const file = await (fileInput as File).text();
+        const photo = new Photos('abc', file);
+        photos.push(photo);
+    }
+
+    const promises = photos.map(async (photo) => {
+        const image = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(photo),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        return image;
+    });
+
+    const responses = await Promise.all(promises);*/
+	};
+</script>
+
+<body>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/aws-sdk/2.1447.0/aws-sdk.min.js"></script>
+</body>
--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,95 +1,49 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { get } from '$lib/db';
-	import Carousel from '$lib/components/Carousel.svelte';
 	import process from 'process';
 	import { encode, decode } from 'js-base64';
-	import { compress } from 'image-conversion';
+	import Title from '$lib/components/Title.svelte';
+	import { handleSubmit } from '$lib/photos.svelte';
 	import './style.scss';
 
 	let photosURL: string[] = [];
 	//SF/4OVvE5KvU57QNjYz4y0IlMMx7IhhvFbFSO5WY
-	const dontReadThisPls = encode('SF/4OVvE5KvU57QNjYz4y0IlMMx7IhhvFbFSO5WY');
 
-	const handleSubmit = async (e: SubmitEvent) => {
-		e.preventDefault();
-
-		const formData = new FormData(e.target as HTMLFormElement);
-		const file = formData.get('file') as Blob;
-
-		// Assuming 'file' is your PNG image file
-		let jpgBlob: Blob;
-		await compress(file, {
-			quality: 0.8,
-			type: 'image/jpeg'
-		}).then((res: Blob) => {
-			console.log(res);
-			jpgBlob = res;
-		});
-
-		AWS.config.update({
-			accessKeyId: 'AKIAWQZV5LCLPYYRFKGN',
-			secretAccessKey: decode(dontReadThisPls),
-			region: 'us-east-1'
-		});
-
-		const s3 = new AWS.S3();
-
-		const params = {
-			Bucket: 'file-upload-sh',
-			Key: 'image.jpg',
-			Body: jpgBlob
-		};
-
-		s3.upload(params, (err, data) => {
-			if (err) {
-				console.error('Error uploading file:', err);
-			} else {
-				console.log('File uploaded successfully. S3 URL:', data.Location);
-			}
-		});
-		/*const url = 'https://5gkaf9hvg6.execute-api.us-east-1.amazonaws.com/photos';
-		const formData = new FormData(e.target as HTMLFormElement);
-
-		const fileInputs = formData.getAll('file');
-
-		let photos = [];
-
-		for (const fileInput of fileInputs) {
-			const file = await (fileInput as File).text();
-			const photo = new Photos('abc', file);
-			photos.push(photo);
-		}
-
-		const promises = photos.map(async (photo) => {
-			const image = await fetch(url, {
-				method: 'POST',
-				body: JSON.stringify(photo),
-				headers: {
-					'Content-Type': 'application/json'
-				}
-			});
-			return image;
-		});
-
-		const responses = await Promise.all(promises);*/
+	const SubmitFirst = async (e: SubmitEvent) => {
+		await handleSubmit(e, '1');
+	};
+	const SubmitSecond = async (e: SubmitEvent) => {
+		await handleSubmit(e, '2');
+	};
+	const SubmitThird = async (e: SubmitEvent) => {
+		await handleSubmit(e, '3');
+	};
+	const SubmitForth = async (e: SubmitEvent) => {
+		await handleSubmit(e, '4');
+	};
+	const SubmitFifth = async (e: SubmitEvent) => {
+		await handleSubmit(e, '5');
 	};
 
+	let photos: object[] = [{}];
 	onMount(async () => {
 		try {
-			const response = await fetch('https://file-upload-sh.s3.amazonaws.com/image.jpg');
-			const blob = await response.blob();
-			//let photoOut = await response.json();
+			for (let i = 1; i < 6; i++) {
+				const response = await fetch('https://file-upload-sh.s3.amazonaws.com/' + i + '.jpg');
+				const blob = await response.blob();
+				//let photoOut = await response.json();
 
-			console.log(blob);
-			photosURL.push(URL.createObjectURL(blob));
-
-			photosURL = photosURL; //...
-			console.log(photosURL);
+				console.log(blob);
+				photosURL.push(URL.createObjectURL(blob));
+			}
 		} catch (error) {
 			console.error('Error loading images:', error);
 		}
 		//shopsArr = shops.map((shop: { [x: string]: object; }) => {return shop['name']})
+		photosURL = photosURL;
+		console.log(photosURL);
+		photos = await get('photos');
 	});
 
 	let aaa: FileList;
@@ -97,19 +51,35 @@
 
 <body>
 	<section>
-		<form on:submit|preventDefault={handleSubmit}>
-			<label class="aaa secondary-button"
-				>{#if aaa == undefined}
-					Změň obrázek
-				{/if}{#if aaa != undefined}Obrázek vybrán ✔️{/if}
-				<input name="file" type="file" accept="image/png, image/jpeg" bind:files={aaa} />
-			</label>
-			<button type="submit">Upload</button>
-		</form>
-		{#each photosURL as photoURL}
-			<p>{photoURL}</p>
-			<img src={photoURL} alt="" />
-		{/each}
+		<div class="card">
+			<h1 style="color: white;">Kolotoč obrázek 1</h1>
+			<form on:submit|preventDefault={SubmitFirst}>
+				<label class="imageSelector primary-button"
+					>{#if aaa == undefined}
+						Změň obrázek
+					{/if}{#if aaa != undefined}Obrázek vybrán ✔️{/if}
+					<input name="file" type="file" accept="image/png, image/jpeg" bind:files={aaa} />
+				</label>
+				<button type="submit">Nahrát obrázek</button>
+			</form>
+
+			<img src={photosURL[0]} alt="" />
+			<p>popisek:</p>
+			{#if photos[0].id != undefined}
+				<form action="?/photo" method="post">
+					<textarea
+						name="desc"
+						maxlength="100"
+						class="desc"
+						value={photos.filter((a) => a.name === '1')[0].desc}
+					/>
+					<label
+						>odkaz:
+						<input name="url" type="url" />
+					</label>
+					<button formaction="?/photo">Potvrdit popisek a odkaz</button>
+				</form>
+			{/if}
+		</div>
 	</section>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/aws-sdk/2.1447.0/aws-sdk.min.js"></script>
 </body>
