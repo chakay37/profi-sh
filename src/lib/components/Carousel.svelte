3a83a63887856a76eb4c0ef0b8fdef95bc296009
David Pýcha <davidpycha1@gmail.com>
2024-08-31 19:06:14 +0200

    vetsi carousel

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { get } from '$lib/db';
 	import { onMount } from 'svelte';
-	import Carousel from 'svelte-carousel'
+	import Carousel from 'svelte-carousel';
 
 	async function getFilteredPhotos(photos) {
 		for (let i = 1; i < 6; i++) {
@@ -20,7 +20,6 @@
 
 			photo.photoURL = URL.createObjectURL(blob);
 			photosFiltered.push(photo);
-
 		}
 		return photosFiltered;
 	}
@@ -33,10 +32,10 @@
 
 	let carousel;
 	function goToPrevPage() {
-		carousel.goToPrev()
+		carousel.goToPrev();
 	}
 	function goToNextPage() {
-		carousel.goToNext()
+		carousel.goToNext();
 	}
 </script>
 
@@ -46,51 +45,60 @@
 </head>
 <body>
 	<div class="carousel-wrapper">
-	{#if photosFiltered.length > 4}
-		<div class="carousel-container">
-		<Carousel
-		bind:this={carousel}
-			autoplay
-			autoplayDuration={3000}
-		>
-		<div slot="prev">
-			<div  class="prev-container" on:click={goToPrevPage}>
-			<svg fill="#ffffff" width="20px" height="20px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
-				<path d="M23.505 0c0.271 0 0.549 0.107 0.757 0.316 0.417 0.417 0.417 1.098 0 1.515l-14.258 14.264 14.050 14.050c0.417 0.417 0.417 1.098 0 1.515s-1.098 0.417-1.515 0l-14.807-14.807c-0.417-0.417-0.417-1.098 0-1.515l15.015-15.022c0.208-0.208 0.486-0.316 0.757-0.316z"></path>
-			</svg></div>
-		  </div>
-		  <div  slot="next">
-			<div class="next-container" on:click={goToNextPage}>
-			<svg fill="#ffffff" width="20px" height="20px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
-				<path d="M8.489 31.975c-0.271 0-0.549-0.107-0.757-0.316-0.417-0.417-0.417-1.098 0-1.515l14.258-14.264-14.050-14.050c-0.417-0.417-0.417-1.098 0-1.515s1.098-0.417 1.515 0l14.807 14.807c0.417 0.417 0.417 1.098 0 1.515l-15.015 15.022c-0.208 0.208-0.486 0.316-0.757 0.316z"></path>
-			</svg>
-		</div>
-		  </div>
-			{#each photosFiltered as photo}
-				<a href={photo.url} target="_blank">
-						<div class="carousel-item">
-							<img
-								src={photo.photoURL}
-								alt="obrázek oblečení"
-							/>
-							{#if photo.desc !== undefined && photo.desc !== '' && photo.desc !== null}
-								<div class="carousel-caption">
-									<p>
-										{photo.desc}
-									</p>
-								</div>
-
-							{/if}
-
+		{#if photosFiltered.length > 4}
+			<div class="carousel-container">
+				<Carousel bind:this={carousel} autoplay autoplayDuration={3000}>
+					<div slot="prev">
+						<div class="prev-container" on:click={goToPrevPage}>
+							<svg
+								fill="#ffffff"
+								width="20px"
+								height="20px"
+								viewBox="0 0 32 32"
+								version="1.1"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									d="M23.505 0c0.271 0 0.549 0.107 0.757 0.316 0.417 0.417 0.417 1.098 0 1.515l-14.258 14.264 14.050 14.050c0.417 0.417 0.417 1.098 0 1.515s-1.098 0.417-1.515 0l-14.807-14.807c-0.417-0.417-0.417-1.098 0-1.515l15.015-15.022c0.208-0.208 0.486-0.316 0.757-0.316z"
+								/>
+							</svg>
 						</div>
-				</a>
-			{/each}
-			<div slot="dots">
-			  </div>
-		</Carousel>
-		</div>
-	{/if}
-</div>
+					</div>
+					<div slot="next">
+						<div class="next-container" on:click={goToNextPage}>
+							<svg
+								fill="#ffffff"
+								width="20px"
+								height="20px"
+								viewBox="0 0 32 32"
+								version="1.1"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									d="M8.489 31.975c-0.271 0-0.549-0.107-0.757-0.316-0.417-0.417-0.417-1.098 0-1.515l14.258-14.264-14.050-14.050c-0.417-0.417-0.417-1.098 0-1.515s1.098-0.417 1.515 0l14.807 14.807c0.417 0.417 0.417 1.098 0 1.515l-15.015 15.022c-0.208 0.208-0.486 0.316-0.757 0.316z"
+								/>
+							</svg>
+						</div>
+					</div>
+					{#each photosFiltered as photo}
+						<a href={photo.url} target="_blank">
+							<div class="carousel-item">
+								<img src={photo.photoURL} alt="obrázek oblečení" />
+								{#if photo.desc !== undefined && photo.desc !== '' && photo.desc !== null}
+									<div class="carousel-caption">
+										<p>
+											{photo.desc}
+										</p>
+									</div>
+								{/if}
+							</div>
+						</a>
+					{/each}
+					<div slot="dots" />
+				</Carousel>
+			</div>
+		{/if}
+	</div>
 </body>
 
 <style lang="scss">
@@ -99,19 +107,13 @@
 	$logo-color-light: #b6a771;
 	$white: #f1f1ee;
 
-	.carousel-wrapper {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
-
 	.carousel-container {
 		width: 290px;
 		overflow-y: hidden;
 		height: 300px;
 		a {
-      text-decoration: none;
-    }
+			text-decoration: none;
+		}
 		img {
 			width: 150px;
 			height: 150px;
@@ -122,7 +124,7 @@
 		}
 		.carousel-caption {
 			p {
-			font-size: 16px;
+				font-size: 16px;
 			}
 			border-radius: 5px;
 			background-color: $logo-color-dark;
@@ -142,28 +144,27 @@
 		}
 	}
 	@media (min-width: 500px) {
+		//	.carousel-container {
+		//		width: 420px;
+		//		height: 420px;
+		//		img {
+		//			width: 280px;
+		//			height: 280px;
+		//		}
+		//
+		//			.prev-container,
+		//			.next-container {
+		//				margin-top: 130px;
+		//			}
+		//		}
+		//	}
+		//	@media (min-width: 1400px) {
 		.carousel-container {
-			width: 420px;
-			height: 420px;
+			min-width: 550px;
+			min-height: 550px;
 			img {
-				width: 280px;
-				height: 280px;
-			}
-
-			.prev-container,
-			.next-container {
-				margin-top: 130px;
-			}
-		}
-		
-	}
-	@media (min-width: 1400px) {
-		.carousel-container {
-			width: 500px;
-			height: 500px;
-			img {
-				width: 360px;
-				height: 360px;
+				width: 410px;
+				height: 410px;
 			}
 
 			.prev-container,
@@ -172,5 +173,4 @@
 			}
 		}
 	}
-	
 </style>
