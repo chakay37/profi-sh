601f4a0a6038b14b7d1f5ebd19a1ab7e86fb370f
pycd01 <pycd01@vse.cz>
2026-03-25 00:07:34 +0100

    env api

--- src/routes/api/deals/+server.ts
+++ src/routes/api/deals/+server.ts
@@ -1,7 +1,7 @@
 import { json } from '@sveltejs/kit';
 
 export async function GET() {
-	const res = await fetch('http://localhost:3000/deals');
+	const res = await fetch(process.env.API_URL + '/deals');
 	const data = await res.json();
 	return json(data);
 }
--- src/routes/api/shops/+server.ts
+++ src/routes/api/shops/+server.ts
@@ -1,7 +1,7 @@
 import { json } from '@sveltejs/kit';
 
 export async function GET() {
-	const res = await fetch('http://localhost:3000/shops');
+	const res = await fetch(process.env.API_URL + '/shops');
 	const data = await res.json();
 	return json(data);
 }
