9082928202e84e1f567af9bb5b5920557c092f0f
pycd01 <pycd01@vse.cz>
2026-03-24 21:26:08 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	preview: {
+		host: true,
+		port: process.env.PORT,
+		allowedHosts: ['igs8c0w4cwocgwggs8w8o4sc.46.225.188.172.sslip.io']
+	}
 });
