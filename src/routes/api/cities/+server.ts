f39979e8349fa3d83916255c4161941d2545b3b2
pycd01 <pycd01@vse.cz>
2026-03-24 22:39:11 +0100

    json fix

--- src/routes/api/cities/+server.ts
+++ src/routes/api/cities/+server.ts
@@ -1,5 +1,7 @@
 import { json } from '@sveltejs/kit';
 
 export async function GET() {
-	return await fetch('http://localhost:3000/cities');
+	const res = await fetch('http://localhost:3000/cities');
+	const data = await res.json();
+	return json(data);
 }
--- src/routes/api/photos/+server.ts
+++ src/routes/api/photos/+server.ts
@@ -1,5 +1,7 @@
 import { json } from '@sveltejs/kit';
 
 export async function GET() {
-	return await fetch('http://localhost:3000/photos');
+	const res = await fetch('http://localhost:3000/photos');
+	const data = await res.json();
+	return json(data);
 }
