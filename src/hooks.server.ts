b90e5a47f17e42fdd7e4f2454e3f386182c1d694
pycd01 <pycd01@vse.cz>
2026-04-24 00:55:02 +0200

    Update hooks.server.ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,11 +2,20 @@ import { env } from '$env/dynamic/private';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	if (event.url.pathname.startsWith('/api')) {
+	const { pathname } = event.url;
+	const { method } = event.request;
+
+	const isProtected =
+		(pathname.startsWith('/api/users') && method === 'GET') ||
+		(pathname.startsWith('/api/deals') && ['POST', 'PUT', 'DELETE'].includes(method)) ||
+		pathname.startsWith('/admin');
+
+	if (isProtected) {
 		const auth = event.cookies.get('adminLogin');
 		if (auth !== env.ADMIN_PASSWORD) {
 			return new Response('Unauthorized', { status: 401 });
 		}
 	}
+
 	return resolve(event);
 };
