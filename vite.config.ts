fd4780d3fcd76f3531e78f8c839960bd8dcd2193
pycd01 <pycd01@vse.cz>
2026-04-23 22:18:11 +0200

    add domain to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ export default defineConfig({
 	preview: {
 		host: true,
 		port: process.env.PORT,
-		allowedHosts: ['igs8c0w4cwocgwggs8w8o4sc.46.225.188.172.sslip.io']
+		allowedHosts: [
+			'igs8c0w4cwocgwggs8w8o4sc.46.225.188.172.sslip.io',
+			'profi-sh.cz',
+			'www.profi-sh.cz'
+		]
 	}
 });
